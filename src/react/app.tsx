f1030c69c2be5f5d8298e3ad46253740bdec8b4a
Ra6nar0k21 | Michael <38357618+michaeltrosin@users.noreply.github.com>
2021-08-03 17:07:12 +0200

    Update and rename app.ts to app.tsx

---
+++ src/react/app.tsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class App extends Component<any, any> {
+    render() {
+        return (
+            <div id="app">
+              Hello World!
+            </div>
+        );
+    }
+}
+
+export default App;
