c31bad36db51a7ab66ef4e1d2f855b65d1df5476
Ra6nar0k21 | Michael <38357618+michaeltrosin@users.noreply.github.com>
2021-08-03 13:07:00 +0200

    Create main.ts

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import {app, BrowserWindow, MessageBoxOptions} from 'electron';
+import isDev from 'electron-is-dev'; // New Import
+
+const createWindow = (): void => {
+    let win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true
+        }
+    });
+    win.loadURL(
+        isDev
+            ? 'http://localhost:9000'
+            : `file://${__dirname}/index.html`,
+    );
+}
+
+app.on('ready', createWindow);
